24120853a51a9bd7109ff901729923016227ad02
daniel shelton <danielshelton90@gmail.com>
2021-07-12 15:34:51 -0700

    Displays age in minutes

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -26,16 +26,23 @@ class MainActivity : AppCompatActivity() {
         val year = myCalendar.get(Calendar.YEAR);
         val month = myCalendar.get(Calendar.MONTH);
         val day = myCalendar.get(Calendar.DAY_OF_MONTH);
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
+        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
                 view, selectedYear, selectedMonth, selectedDayOfMonth ->
             Toast.makeText(this, "The chosen year is $selectedYear, the month is ${selectedMonth+1}, the day is $selectedDayOfMonth", Toast.LENGTH_LONG).show()
             val selectedDate = "${selectedMonth + 1}/$selectedDayOfMonth/$selectedYear"
             val sdf = SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH)
             val theDate = sdf.parse(selectedDate)
+            val selectedDateInMinutes = theDate!!.time / 60000
+            val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+            val currentDateInMinutes = currentDate!!.time / 60000
+            val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
             tvSelectedDate.setText(selectedDate)
+            tvSelectedDateInMinutes.setText(differenceInMinutes.toString())
         },
             year,
             month,
-            day).show()
+            day)
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
     }
 }
